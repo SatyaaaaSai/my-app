985c2692ad1bf31209c65ab0bb3926d487a4f586
Satya Sai <satya07thota@gmail.com>
2024-01-25 07:58:18 +0530

    second-commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { React } from "react";
+import "./components/css/App.css";
+import Header from "./components/NotesComponets/Header";
+const App =()=>{
+    return (
+        <div className="main">
+            <Header/>
+        </div>
+    )
+}
+
+export default App;

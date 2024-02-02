b1d608b408ede07fb2db01b26cad83d4f6f20f86
Satya Sai <satya07thota@gmail.com>
2024-02-02 16:02:28 +0530

    final

--- src/components/NotesComponets/App.js
+++ src/components/NotesComponets/App.js
@@ -8,21 +8,23 @@ function App() {
   const [weatherData, setWeatherData] = useState(null);
   const [error, setError] = useState(null);
   const [forecast, setForecast] = useState(false);
-  const [forecastCity,setForecastCity]=useState("");
-  const [isCelcius,setIscelcius]=useState(true);
+  const [forecastCity, setForecastCity] = useState("");
+  const [isCelcius, setIscelcius] = useState(true);
 
   const fetchWeatherData = async () => {
     try {
-      const response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+      const response = await fetch(
+        `${API_URL}?q=${city}&appid=${API_KEY}&units=metric`
+      );
       const data = await response.json();
       console.log(data);
-      if (data.cod === "404" || city === '') {
-          setError("No data Found");
+      if (data.cod === "404" || city === "") {
+        setError("No data Found");
       } else {
         setWeatherData(data);
       }
     } catch (error) {
-      console.error('Error fetching weather data:', error);
+      console.error("Error fetching weather data:", error);
     }
   };
 
@@ -31,94 +33,106 @@ function App() {
     fetchWeatherData();
   };
 
-  const handleToggleForecast= ()=>{
-    if(!city){
+  const handleToggleForecast = () => {
+    if (!city) {
       setError("please enter the valid name in the search Box");
       return;
     }
     setForecast(!forecast);
-    if(!forecast){
+    if (!forecast) {
       setForecastCity(city);
     } else {
       setForecastCity("");
     }
   };
 
-  const toggleTemperatureUnit=()=>{
-    setIscelcius((prevValue)=>!prevValue);
-  }
+  const toggleTemperatureUnit = () => {
+    setIscelcius((prevValue) => !prevValue);
+  };
 
   return (
-    <center>
-    <div className="weather-app">
-      <h1>Weather App</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={city}
-          onChange={(e) => setCity(e.target.value)}
-          placeholder="Enter city name"
+    <div className="mainContainer">
+      <div className="weather-app">
+        <h1>Weather App</h1>
+        <br></br>
+        <img
+          src="https://ayushkul.github.io/react-weather-app/icons/perfect-day.svg"
+          alt="weatherimage"
+          width={"150px"}
+          height={"150px"}
         />
-        <button type="submit">Get Weather</button>
-      </form>
-
-      {error ? (
-        <div>
-          <h1>{error}</h1>
+        <br></br>
+        <div className="txt-field">
+          <form onSubmit={handleSubmit}>
+            <input
+              className="input-box"
+              type="text"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+              placeholder="Enter city name"
+            />
+            <button type="submit" className="search-btn">Search</button>
+          </form>
         </div>
-      ) : (
-        <div>
-          {weatherData && (
-            <div className="weather-details" id="txt">
-              <h2>{weatherData.name}</h2>
-              <p>
-                Temperature: {""}
-                {
-                  isCelcius ? `${weatherData.main.temp}°C`:`${(weatherData.main.temp*9)/5+32}°F`
-                }
-              </p>
-              <p>Min Temperature: {""}
-                {
-                  isCelcius ? `${weatherData.main.temp_min}°C`:`${(weatherData.main.temp_min*9)/5+32}°F`
-                }
-              </p>
-              <p>
-                Max Temperature: {""}
-                {
-                  isCelcius ? `${weatherData.main.temp_max}°C`:`${(weatherData.main.temp_max*9)/5+32}°F`
-                }
-              </p>
-              <p>Humidity: {weatherData.main.humidity}%</p>
-              <p>Wind Speed: {weatherData.wind.speed} m/s</p>
-              <p>Weather: {weatherData.weather[0].description}</p>
-              <img
-                src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
-                alt="Weather Icon"
-              />
-            </div>
-          )}
-        </div>
-      )}
-      {/* done the Main Component */}
-      <div>
-            <button onClick={toggleTemperatureUnit}>
-                {isCelcius ? "switch to Farenheit" : "Switch to celecius"}
-            </button>
-      </div>
-      <div>
-        <label>Show 5-Day Forecast:</label>
-        <button
-          className={forecast ? "toggle-button active" : "toggle-button"}
-          onClick={handleToggleForecast}
-        >
-          {forecast ? "Hide" : "Show"}
-        </button>
-      </div>
-      <div>
-        {forecast &&< WeatherForecast city={forecastCity}/>}
+        <br></br>
+        {error ? (
+          <div>
+            <h1>{error}</h1>
+          </div>
+        ) : (
+          <div>
+            {weatherData && (
+              <div className="weather-details" id="txt">
+                <h2>{weatherData.name}</h2>
+                <p>
+                  Temperature: {""}
+                  {isCelcius
+                    ? `${weatherData.main.temp}°C`
+                    : `${(weatherData.main.temp * 9) / 5 + 32}°F`}
+                </p>
+                <p>
+                  Min Temperature: {""}
+                  {isCelcius
+                    ? `${weatherData.main.temp_min}°C`
+                    : `${(weatherData.main.temp_min * 9) / 5 + 32}°F`}
+                </p>
+                <p>
+                  Max Temperature: {""}
+                  {isCelcius
+                    ? `${weatherData.main.temp_max}°C`
+                    : `${(weatherData.main.temp_max * 9) / 5 + 32}°F`}
+                </p>
+                <p>Humidity: {weatherData.main.humidity}%</p>
+                <p>Wind Speed: {weatherData.wind.speed} m/s</p>
+                <p>Weather: {weatherData.weather[0].description}</p>
+                <img
+                  src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
+                  alt="Weather Icon"
+                />
+                <div>
+                  <button onClick={toggleTemperatureUnit}>
+                    {isCelcius ? "switch to Farenheit" : "Switch to celecius"}
+                  </button>
+                </div>
+                <div>
+                  <label>Show 5-Day Forecast:</label>
+                  <button
+                    className={
+                      forecast ? "toggle-button active" : "toggle-button"
+                    }
+                    onClick={handleToggleForecast}
+                  >
+                    {forecast ? "Hide" : "Show"}
+                  </button>
+                </div>
+                <div>{forecast && <WeatherForecast city={forecastCity} />}</div>
+              </div>
+            )}
+          </div>
+        )}
+        {/* done the Main Component */}
       </div>
     </div>
-    </center>
   );
 }
 
